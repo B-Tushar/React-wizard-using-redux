1b9d57398a9aab6742ba222fb4af9071ef7a692e
Tushar Borawake <borawaketushar21@gmail.com>
2017-07-05 10:53:17 +0530

    Updated index.js
    Done some code formatting.

--- dev/js/reducers/index.js
+++ dev/js/reducers/index.js
@@ -5,31 +5,25 @@ const initialState = {
   currentStep:1
 }
 
-const wizardReducer = (state = initialState,action) => {
-   debugger;
-switch(action.type){
-   case "SAVE_RECORD":
-   return Object.assign({}, state, {
-									  name:action.payload.name,
-								      email:action.payload.email,
-                                      password:action.payload.password,
-                                      currentStep:getNextStep(state)
-									});
-   case "NEXT_STEP":return Object.assign({}, state, {currentStep: getNextStep(state)});
-   case "PREV_STEP":return  Object.assign({}, state, {currentStep: getPrevStep(state)});
-}
+const wizardReducer = (state = initialState, action) => {
+	switch(action.type){
+	   case "SAVE_RECORD":
+			       return Object.assign({}, state, 
+				      {name:action.payload.name,email:action.payload.email,
+				       password:action.payload.password,currentStep:getNextStep(state)});
+
+	   case "NEXT_STEP":
+			       return Object.assign({}, state, {currentStep: getNextStep(state)});
+
+	   case "PREV_STEP":
+			       return  Object.assign({}, state, {currentStep: getPrevStep(state)});
+	}
    return state;
 }
 
 
-const getNextStep = (state) =>{
-	debugger;
-	return ++state.currentStep ;
-}
+const getNextStep = (state) => {return ++state.currentStep}
 
-const getPrevStep = (state) =>{
-  debugger;
-  return --state.currentStep;
-}
+const getPrevStep = (state) => {return --state.currentStep}
 
 export default wizardReducer;
