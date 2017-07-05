00594737bfdb2034e3c6d4ec30aea9d27b99dd30
Tushar Borawake <borawaketushar21@gmail.com>
2017-07-05 10:41:17 +0530

    Updated App.js
    Done some formatting

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -2,19 +2,17 @@ import React from 'react';
 import Account from './Account';
 import Confirmation from './Confirmation';
 import Success from './Success';
-//import {ProgressBar,bsStyle,text,variation} from 'react-bootstrap';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import {connect} from 'react-redux';
-//require('../../scss/style.scss');
 
 class App extends React.Component{
 
 	showStep(){
 		const step = this.props.stateInfo.currentStep;
 		switch(step){
-			case 1: return <Account />
-			case 2: return <Confirmation/>
-            case 3: return <Success/>
+		case 1: return <Account />
+		case 2: return <Confirmation/>
+		case 3: return <Success/>
 		}
 	}
 
@@ -22,25 +20,24 @@ class App extends React.Component{
 		var style = {width : (this.props.stateInfo.currentStep / 3 * 100) + '%'}
 		return(
 			 <div className="container">
-                       <center><h2>Account Details</h2></center><br/><br/>
-                       <div className="row">
+                         <center><h2>Account Details</h2></center><br/><br/>
+                         <div className="row">
                            <div className="col-md-06" style={{marginLeft:"14px"}}>
-					       		<div className="progress-step ">Step {this.props.stateInfo.currentStep}</div>
-                            </div>
-                            <div className="col-md-06" style={{marginLeft:"14px"}}>
-					       		<progress className="progress" value={this.props.stateInfo.currentStep / 3 * 100} max="100" style={style}></progress>
-                            </div>
-					   </div>
-					   <div className="row">
-				       		{this.showStep()}
-					   </div>
+				<div className="progress-step ">Step {this.props.stateInfo.currentStep}</div>
+                           </div>
+                           <div className="col-md-06" style={{marginLeft:"14px"}}>
+				<progress className="progress" value={this.props.stateInfo.currentStep / 3 * 100} 
+			                                                      max="100" style={style}></progress>
+                           </div>
+			</div>
+		           <div className="row">
+			     {this.showStep()}
+		           </div>
 			 </div>
 			);
 	}
 }
 
-const mapStateToProps = (state) => {
-	return {stateInfo : state}
-}
+const mapStateToProps = (state) => {return {stateInfo : state}}
 
 export default connect(mapStateToProps,null)(App);
