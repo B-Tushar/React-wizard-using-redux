a692914523681bfcbfd8b0e97e80f0697e1f57a6
Tushar Borawake <borawaketushar21@gmail.com>
2017-07-05 10:36:20 +0530

    updated index.js
    formatted code for simplicity.

--- dev/js/actions/index.js
+++ dev/js/actions/index.js
@@ -1,23 +1,13 @@
 import * as types from './actionTypes';
 
 export const saveRecord = (data) => {
-	debugger;
-	return{
-			type:types.SAVE_RECORD,
-			payload:data
-		   }
+	return{	type:types.SAVE_RECORD,	payload:data } 
 }
 
 export const nextStep = () => {
-	debugger
-	return{
-			type:types.NEXT_STEP
-		   }
+	return{type:types.NEXT_STEP}
 }
 
 export const prevStep = () => {
-	debugger
-	return{
-			type:types.PREV_STEP
-		   }
+	return{type:types.PREV_STEP}
 }
