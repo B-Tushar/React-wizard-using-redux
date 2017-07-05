1e71c4ded899432df2ff73802a628d1bb4e5e1c1
Tushar Borawake <borawaketushar21@gmail.com>
2017-07-05 10:47:46 +0530

    Updated Confirmation.js
    Done some code formatting.

--- dev/js/components/Confirmation.js
+++ dev/js/components/Confirmation.js
@@ -13,19 +13,14 @@ class Confirmation extends Component{
                     <div className="panel-body">
                      <form className="form-horizontal">
                         <div className="form-group form-group-md">
-
-                         <div className="row">
-                              <label style={{ marginLeft:'15%'}}>Name:</label>      {this.props.fieldValues.name}
-                              <br/>
-                              <label style={{ marginLeft:'15%'}}>Email:</label>      {this.props.fieldValues.email}
-                          </div>
-                              <br/>
-
-                          <div className="row">
-                                <input type="button" className="btn btn-primary" onClick={()=>this.props.prevStep()} value="Back" style={{ marginLeft:'15%'}}/>
-                                <input type="button" className="btn btn-primary" onClick={()=>this.props.nextStep()} value="Submit Registration" style={{ marginLeft:'2%'}}/>
-                          </div><br/>
-
+                           <div className="row">
+                                <label style={{ marginLeft:'15%'}}>Name:</label> {this.props.fieldValues.name}<br/>
+                                <label style={{ marginLeft:'15%'}}>Email:</label> {this.props.fieldValues.email}
+                            </div><br/>
+                            <div className="row">
+                                  <input type="button" className="btn btn-primary" onClick={()=>this.props.prevStep()} value="Back" style={{ marginLeft:'15%'}}/>
+                                  <input type="button" className="btn btn-primary" onClick={()=>this.props.nextStep()} value="Submit Registration" style={{ marginLeft:'2%'}}/>
+                            </div><br/>
                           </div>
                       </form>
                     </div>
@@ -36,13 +31,9 @@ class Confirmation extends Component{
   }
 }
 
-const mapStateToProps = (state, props) => {
-  return {fieldValues:state}
-}
+const mapStateToProps = (state, props) => {return {fieldValues:state}}
 
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({nextStep,prevStep},dispatch);
-}
+const mapDispatchToProps = (dispatch) => {return bindActionCreators({nextStep,prevStep},dispatch)}
 
 
 export default connect(mapStateToProps,mapDispatchToProps)(Confirmation);
